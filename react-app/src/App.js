79ff01d5d4df95bd03d8460deab6daa1497c1ef9
Vinicius Eduardo <vinicius.eddu99@gmail.com>
2022-08-06 20:32:49 -0300

    instalado react-router dom@v5

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,12 +1,15 @@
-import './App.css';
-import Header from './components/Header';
+import React, { Component } from "react";
+import "./App.css";
+import Header from "./components/Header";
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
+  }
 }
 
 export default App;
