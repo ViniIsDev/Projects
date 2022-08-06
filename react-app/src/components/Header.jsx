7c99da2b08942034ee900ba57a9c30cca88a2d26
Vinicius Eduardo <vinicius.eddu99@gmail.com>
2022-08-06 19:50:33 -0300

    primeiro commit

---
+++ react-app/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class Header extends Component {
+    render() {
+        return <header><h1>Cabeçalho Aqui!</h1></header>
+    }
+};
+
+export default Header;
